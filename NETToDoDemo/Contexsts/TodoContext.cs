06248ed5ff4934b53bca778f6b722b82603adcbb
slava-pc\Slava <slavasotnikov2020@ukr.net>
2022-10-16 18:35:32 +0300

    CreatedToDo

---
+++ NETToDoDemo/Contexsts/TodoContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NETToDoDemo.Models;
+
+namespace NETToDoDemo.Contexsts
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base(options) {}
+
+        public DbSet<ToDo> ToDos { get; set; }
+    }
+}
